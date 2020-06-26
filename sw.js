930ff394df34b8aaee9ba32dd0c97ee826f33a66
Rodrigo Tucuman <rembert.rodrigo@gmail.com>
2020-06-26 06:33:46 -0400

    se comento la estrategia cache only

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 self.addEventListener('install', e => {
 
-    const cacheProm = caches.open('cache-1')
+    const cacheProm = caches.open('cache-v1')
         .then(cache => {
             cache.addAll([
-                //'/',
+                '/',
                 'index.html',
                 'css/style.css',
                 'img/main.jpg',
                 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
                 'js/app.js'
-            ]); 
+            ]);
         });
     e.waitUntil(cacheProm); 
 });
 
-self.addEventListener('fetch', e =>{
+/* self.addEventListener('fetch', e =>{
     //1- cache only
     e.respondWith(caches.match(e.request));
-});
+}); */
