9736b156c098603a5d4f3cce1379bfc5ad420804
Rodrigo Tucuman <rembert.rodrigo@gmail.com>
2020-07-02 11:08:32 -0400

    estrategia cache with network update

--- sw.js
+++ sw.js
@@ -41,25 +41,40 @@ self.addEventListener('install', e => {
 });
 
  self.addEventListener('fetch', e =>{
+
+    //4.- cache with netwok update
+    //cuando el rendimiento es critico, q aprezca lo mas rapido posible
+    //siempre estara un version atras a una actualizacion
+
+    if(e.request.url.includes('bootstrap')){ //evaluacion del bootstrap
+        return e.respondWith(caches.match(e.request));
+    }
+    const respuesta = caches.open(CACHE_STATIC_NAME)
+        .then(cache=>{
+            fetch(e.request).then(newRes=>
+                cache.put(e.request, newRes));
+            return cache.match(respuesta);
+        });
+
     //3- network with cache fallback
     //busca en inter el recurso y si no hay busca en el cache
 
-    const respuesta = fetch(e.request).then(res =>{
+    // const respuesta = fetch(e.request).then(res =>{
 
-        if(!res) return caches.match(e.request);
+    //     if(!res) return caches.match(e.request); //ultima opcion para recuperar el recurso del cache. esto no me funciona porq no existe en ningun lado el recurso main2 q cambien en el index(imagen)
 
-        caches.open(CACHE_DYNAMIC_NAME)
-            .then(cache =>{
-                cache.put(e.request.res);
-                limpiarCache(CACHE_DYNAMIC_NAME,CACHE_DYNAMIC_LIMIT);
+    //     caches.open(CACHE_DYNAMIC_NAME)
+    //         .then(cache =>{
+    //             cache.put(e.request.res);
+    //             limpiarCache(CACHE_DYNAMIC_NAME,CACHE_DYNAMIC_LIMIT);
 
-            });
+    //         });
         
-        return res.clone();
+    //     return res.clone();
 
-    }).catch(err =>{
-        return caches.match( respuesta );  
-    });
+    // }).catch(err =>{
+    //     return caches.match( respuesta );  
+    // });
     
     //2- cache with network fallback
     //busca first en la cache y sino esta recien
@@ -84,5 +99,5 @@ self.addEventListener('install', e => {
     //1- cache only
     //e.respondWith(caches.match(e.request));
 
-     e.respondWith(respuesta);
+    //  e.respondWith(respuesta);
 }); 
