cc83a3b3d12d5e863962f085e73f1dc0d3ce34e2
Rodrigo Tucuman <rembert.rodrigo@gmail.com>
2020-07-29 03:32:06 -0400

    envio del post al servidor para los mensajes

--- public/js/sw-utils.js
+++ public/js/sw-utils.js
@@ -41,17 +41,28 @@ function actualizaCacheStatico( staticCache, req, APP_SHELL_INMUTABLE ) {
 //estrategia network with cache fallback / update
 function manejoApiMensajes( cacheName, req ) {
 
-    return fetch( req ).then(res => {
+    if ( req.clone().method === 'POST' ) {
+        //POSTEO de un nuevo mensaje
+        
+        // tengo q guardar en el indexDB
+        return fetch (req);
 
-        if ( res.ok ) {
-            actualizaCacheDinamico( cacheName, req, res.clone() );
-            return res.clone();
-        } else {
-            return caches.match( req );
-        }
-    }).catch( err => {
-        return caches.match(req);
-    });
+    }else{
+
+        return fetch( req ).then(res => {
+    
+            if ( res.ok ) {
+                actualizaCacheDinamico( cacheName, req, res.clone() );
+                return res.clone();
+            } else {
+                return caches.match( req );
+            }
+        }).catch( err => {
+            return caches.match(req);
+        });
+
+    }
+
 
 }
 
--- server/routes.js
+++ server/routes.js
@@ -26,6 +26,8 @@ router.post('/', function (req, res) {
 
 mensajes.push( mensaje );
 
+console.log( mensajes );
+
   res.json({
     ok: true,
     mensaje
